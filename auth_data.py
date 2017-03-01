f612293fc3e7fb71aef98f14a25d56e459709639
server <dfaustryjak@gmail.com>
2017-03-01 13:23:13 +0100

    LOL

---
+++ auth_data.py
@@ -0,0 +1,7 @@
+def data_for_auth():
+    data_for_auth= []
+    data_for_auth.append("root")
+    data_for_auth.append("alamakota")
+    data_for_auth.append("10.105.10.196")
+    data_for_auth.append("actions")
+    return data_for_auth
