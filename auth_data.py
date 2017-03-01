a796865114f7e80863974a2f5cfcd0d2a24f74fe
Damian <dfaustryjak@gmail.com>
2017-03-01 13:53:02 +0100

    Delete auth_data.py

--- auth_data.py
+++
@@ -1,7 +0,0 @@
-def data_for_auth():
-    data_for_auth= []
-    data_for_auth.append("root")
-    data_for_auth.append("alamakota")
-    data_for_auth.append("10.105.10.196")
-    data_for_auth.append("actions")
-    return data_for_auth
